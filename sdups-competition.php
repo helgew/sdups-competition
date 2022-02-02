819f4294ec51e2b907bf8a85acc36c27ce0ee47f
Helge Weissig <helgew@grajagan.org>
2022-02-02 13:33:35 -0800

    fixed typo

--- sdups-competition.php
+++ sdups-competition.php
@@ -20,7 +20,7 @@
  * Author URI: https://www.grajagan.org
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       sdups-competiton
+ * Text Domain:       sdups-competition
  * Domain Path:       /languages
  */
 
@@ -34,33 +34,34 @@ if ( ! defined( 'WPINC' ) ) {
  */
 define( 'SDUPS_COMPETITION_VERSION', '1.0.0' );
 define( 'SDUPS_COMPETITION_POST_TYPE', 'sdups-competition' );
+define( 'SDUPS_COMPETITION_USER_CAPABILITY', 'manage_sdups_competitions' );
 
 /**
  * The code that runs during plugin activation.
- * This action is documented in includes/class-sdups-competiton-activator.php
+ * This action is documented in includes/class-sdups-competition-activator.php
  */
-function activate_sdups_competiton() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-sdups-competiton-activator.php';
+function activate_sdups_competition() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-sdups-competition-activator.php';
 	SDUPS_Competition_Activator::activate();
 }
 
 /**
  * The code that runs during plugin deactivation.
- * This action is documented in includes/class-sdups-competiton-deactivator.php
+ * This action is documented in includes/class-sdups-competition-deactivator.php
  */
-function deactivate_sdups_competiton() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-sdups-competiton-deactivator.php';
+function deactivate_sdups_competition() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-sdups-competition-deactivator.php';
 	SDUPS_Competition_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_sdups_competiton' );
-register_deactivation_hook( __FILE__, 'deactivate_sdups_competiton' );
+register_activation_hook( __FILE__, 'activate_sdups_competition' );
+register_deactivation_hook( __FILE__, 'deactivate_sdups_competition' );
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-sdups-competiton.php';
+require plugin_dir_path( __FILE__ ) . 'includes/class-sdups-competition.php';
 
 /**
  * Begins execution of the plugin.
@@ -71,11 +72,11 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-sdups-competiton.php';
  *
  * @since    1.0.0
  */
-function run_sdups_competiton() {
+function run_sdups_competition() {
 
 	$plugin = new SDUPS_Competition();
 	$plugin->run();
 
 }
 
-run_sdups_competiton();
+run_sdups_competition();
